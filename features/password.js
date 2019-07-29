c38068571040885b303bdad012698e5e83f186c4
Duanckham <duanckham@gmail.com>
2019-07-30 00:27:52 +0800

    MISC

--- features/password.js
+++ features/password.js
@@ -1,8 +1,12 @@
+const _ = require('lodash');
 const readline = require('readline');
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+const EMPTY = '';
 
 function mixParameters(parameters) {
   return parameters.reduce((item, property) => {
@@ -33,15 +37,64 @@ function askQuestion(question) {
   });
 }
 
+function upperCaseFirstLetter(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+// Example: Yue Qingyu
+function dismemberingChineseNames(data) {
+  let words = [];
+
+  // yqy
+  words.push(data.map(v => v[0]).join(''));
+
+  // YQY
+  words.push(words[0].toUpperCase());
+
+  // Yqy
+  words.push(upperCaseFirstLetter(words[0]));
+
+  // yueqy
+  words.push(data[0] + words[0].substr(1));
+
+  // Yueqy
+  words.push(upperCaseFirstLetter(words[3]));
+
+  // qingyu
+  words.push(data.slice(1).join(''));
+
+  // Qingyu
+  words.push(upperCaseFirstLetter(words[5]));
+
+  // qy
+  words.push(data.slice(1).map(v => v[0]).join(''));
+
+  // Qy
+  words.push(upperCaseFirstLetter(words[7]));
+
+  return words;
+}
+
+function dismemberingUsedPasswords(data) {
+  let prefixList = [];
+  let suffixList = [];
+  let keywordList = [];
+
+  for (let i = 0; i < data.length; i++) {
+    // yqy123456
+    if (/[\D]+[\d]+/.test(data[i])) {
+      console.log(data[i]);
+    }
+  }
+}
+
+console.log(dismemberingUsedPasswords(['yqy11112222', 'yueqingyu', '123456']));
+
 exports.do = async outputFile => {
   let answers = await ask([
     {
       key: 'actualName',
       question: 'Actual name',
-    },
-    {
-      key: 'networkName',
-      question: 'Network name',
       multiple: true,
     },
     {
@@ -89,5 +142,29 @@ exports.do = async outputFile => {
 
   console.log(answers);
 
-  let seeds = [];
+  let seeds = [
+    [EMPTY], // Prefix
+    [EMPTY], // Key
+    [EMPTY], // Joiner
+  ];
+
+  // Prefix
+  if (answers.possiblePrefix.length) {
+    seeds[0] = seeds[0].concat(answers.possiblePrefix);
+  }
+
+  if (answers.actualName.length) {
+    seeds[0] = seeds[0].concat(dismemberingChineseNames(answers.actualName));
+  }
+
+  if (answers.networkID.length) {
+    seeds[0] = seeds[0].concat(answers.networkID);
+  }
+
+
+
+  // Keyword
+  if (answers.networkName)
+
+  console.log(seeds);
 };
